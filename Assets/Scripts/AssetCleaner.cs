ed2985d4c23904cea0136cec61382232ceee2eee
NETID\brownt4 <junichifujio@gmail.com>
2018-05-04 19:12:25 -0700

    Fix: Project slowdown caused by workflow issue with Unity's Resources, which holds a copy of every created Texture2D and GameObject forever until manually prompted to clear itself. This would cause a massive memory leak as the project continued to create new textures and gameobjects.

---
+++ Assets/Scripts/AssetCleaner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARPortal
+{
+    /// <summary>
+    /// Unity seems to have a backend for their Resources that tracks and holds 
+    /// onto Textures and GameObjects created, without releasing those Textures 
+    /// when they're no longer used. This means that there will always be a 
+    /// reference to generated Texture2Ds. This class just calls 
+    /// Resources.UnloadUnusedAssets repeatedly to clear the project and prevent 
+    /// the project from stacking up too high of a memory cost.
+    /// </summary>
+    public class AssetCleaner : MonoBehaviour
+    {
+        public bool Clean = true;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Clean)
+            {
+                Resources.UnloadUnusedAssets();
+            }
+        }
+    }
+}
